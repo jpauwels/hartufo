e378b8d12d2c37719ef586884d6032a96bf05a89
Johan Pauwels <jpauwels@users.noreply.github.com>
2022-04-14 13:44:19 +0100

    Extend coordinate conversion functions

--- hrtfdata/util.py
+++ hrtfdata/util.py
@@ -13,8 +13,7 @@ def spherical2interaural(azimuth, elevation, radius, angles_in_degrees=True):
         azimuth = np.deg2rad(azimuth)
         elevation = np.deg2rad(elevation)
     lateral = np.arcsin(np.sin(azimuth) * np.cos(elevation))
-    vertical = np.arctan(np.tan(elevation) / np.cos(azimuth))
-    vertical[np.cos(azimuth) < 0] += np.pi
+    vertical = np.arctan2(np.tan(elevation), np.cos(azimuth))
     if angles_in_degrees:
         return np.rad2deg(lateral), np.rad2deg(vertical), radius
     else:
@@ -32,11 +31,50 @@ def spherical2cartesian(azimuth, elevation, radius, angles_in_degrees=True):
     return x, y, z
 
 
-def cartesian2spherical(x, y, z, angle_unit='degrees'):
+def cartesian2spherical(x, y, z, angles_in_degrees=True):
+    x = np.asarray(x)
+    y = np.asarray(y)
+    z = np.asarray(z)
     azimuth = np.arctan2(y, x)
     elevation = np.arctan2(z, np.sqrt(x**2 + y**2))
     radius = np.sqrt(x**2 + y**2 + z**2)
-    if angle_unit == 'degrees':
+    if angles_in_degrees:
         return np.rad2deg(azimuth), np.rad2deg(elevation), radius
     else:
         return azimuth, elevation, radius
+
+
+def cartesian2interaural(x, y, z, angles_in_degrees=True):
+    x = np.asarray(x)
+    y = np.asarray(y)
+    z = np.asarray(z)
+    lateral = np.arctan2(y, np.sqrt(x**2 + z**2))
+    vertical = np.arctan2(z, x) 
+    radius = np.sqrt(x**2 + y**2 + z**2)
+    if angles_in_degrees:
+        return np.rad2deg(lateral), np.rad2deg(vertical), radius
+    else:
+        return lateral, vertical, radius
+
+
+def interaural2spherical(lateral, vertical, radius, angles_in_degrees=True):
+    if angles_in_degrees:
+        lateral = np.deg2rad(lateral)
+        vertical = np.deg2rad(vertical)
+    azimuth = np.arctan2(np.tan(lateral), np.cos(vertical))
+    elevation = np.arcsin(np.cos(lateral) * np.sin(vertical))
+    if angles_in_degrees:
+        return np.rad2deg(azimuth), np.rad2deg(elevation), radius
+    else:
+        return azimuth, elevation, radius
+
+
+def interaural2cartesian(lateral, vertical, radius, angles_in_degrees=True):
+    if angles_in_degrees:
+        lateral = np.deg2rad(lateral)
+        vertical = np.deg2rad(vertical)
+    med_proj = radius * np.cos(lateral)
+    x = med_proj * np.cos(vertical)
+    y = radius * np.sin(lateral)
+    z = med_proj * np.sin(vertical)
+    return x, y, z
